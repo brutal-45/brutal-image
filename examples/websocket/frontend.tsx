c9d780d05c297bf80678935a5730e2715496aaba
Brutal-45 <creatorsports81@gmail.com>
2026-04-11 00:04:01 +0530

    Update frontend.tsx

--- examples/websocket/frontend.tsx
+++ examples/websocket/frontend.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'; 
 import { io } from 'socket.io-client';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
