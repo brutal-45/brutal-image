fabeae85c54d97369bd865e0405a122563bbf901
Brutal-45 <creatorsports81@gmail.com>
2026-04-15 10:34:34 +0530

    Update server.ts

--- examples/websocket/server.ts
+++ examples/websocket/server.ts
@@ -2,7 +2,7 @@ import { createServer } from 'http'
 import { Server } from 'socket.io'
 
 const httpServer = createServer()
-const io = new Server(httpServer, {
+const io = new Server(httpServer, { 
   // DO NOT change the path, it is used by Caddy to forward the request to the correct port
   path: '/',
   cors: {
