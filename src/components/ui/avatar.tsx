22c99b82e8813e04fae8daadb1568f187d5d6e6d
brutal-45 <creatorsports81@gmail.com>
2026-05-06 16:50:12 +0530

    Fix formatting in Avatar component props

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -6,7 +6,7 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar"
 import { cn } from "@/lib/utils"
 
 function Avatar({
-  className,
+  className,          
   ...props
 }: React.ComponentProps<typeof AvatarPrimitive.Root>) {
   return (
