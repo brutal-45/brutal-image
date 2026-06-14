a9835837ad59e11316e1aafd0135c4d9b2b4cf8e
brutal-45 <creatorsports81@gmail.com>
2026-06-15 01:02:46 +0530

    Fix formatting of __filename declaration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url); 
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
